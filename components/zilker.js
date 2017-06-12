4f603083b2edfae5e18c4f386763f66bc3ae55b2
Sarah Goldgar <sgoldgar@gmail.com>
2017-06-12 15:11:53 -0500

    added comments to some code

--- components/zilker.js
+++ components/zilker.js
@@ -1,25 +1,30 @@
 $(document).ready(function() {
+  // hide instructions
   $(".speech-bubble").hide();
   $(".hidden-item").hide();
   $("#second-instructions, #third-instructions, #zilker-hints").hide();
 
+  // when you click the help button, show hints at the bottom
   $('.help-button').click(function(){
     $('#zilker-hints').toggle();
   })
 
+  // the first time you click the backpack, show instructions relating to backpack
   $( ".backpack" ).one('click', function() {
     $("#second-instructions").show();
   });
 
+  // the first time you click the sheep in the backpack, show instructions relating to giving dog items
   $( ".backpack-sheep" ).one('click', function() {
     $("#third-instructions").show();
   });
 
+  // when you click close button, hide the parent instructions
   $( ".close" ).click(function() {
     $(this).parent().hide();
   });
 
-
+  // when you click ginny, if the backpack sheep is highlighted, show completed quest speech bubble and bring item to her, otherwise, just show her normal spech bubble
   $(".ginny" ).click(function() {
     if($('.backpack-sheep').hasClass('highlighted-item')){
       $(".speech-bubble p").text('Thank you for finding my sheep!');
@@ -37,30 +42,27 @@ $(document).ready(function() {
 
   });
 
+  // when you click the sheep toy hidden on the scene, put it in the backpack, hide the sheep on-screen
   $(".sheep-toy").click(function(){
     $( ".sheep-toy" ).animate({
       left: "150px",
       top: "150px",
       display: 'none'
     }, 800, function() {
+      // remove the class hiding the item from the backpack sheep
       $('.backpack-sheep').removeClass('hidden-item').show().delay(2000);
     });
   })
 
-
+  // when you click the sheep in the backpack, highlight and unhighlight item
   $(".backpack-sheep").click(function() {
     $(this).toggleClass('highlighted-item');
   });
 
-
+  // when you click the backpack, show and hide items inside
   $(".backpack").click(function() {
     $(".backpack-items").toggle();
   });
-
-  $( ".help-button" ).click(function() {
-    $("#initial-instructions").toggle();
-  });
-
 
 
 
